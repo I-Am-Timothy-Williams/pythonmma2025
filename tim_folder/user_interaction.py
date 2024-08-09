cb228ef4c0fdc63c61538960c48801df827ed5af
Timothy Williams <timwill001@gmail.com>
2024-08-09 11:04:30 -0400

    update: add user interacions

---
+++ tim_folder/user_interaction.py
@@ -0,0 +1,10 @@
+class UserInteraction:
+    @staticmethod
+    def likeUser(userId,likeduserId):
+        return
+    @staticmethod
+    def dislikeUser(userId,dislikeduserId):
+        return
+    @staticmethod
+    def viewMatches(userId):
+        return
