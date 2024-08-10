9d7949d4f1a0a1ef5438078289a468255ce57150
Timothy Williams <timwill001@gmail.com>
2024-08-10 17:53:43 -0400

    update: add additional tables, adjust schema w foreign keys

--- tim_folder/schema.py
+++ tim_folder/schema.py
@@ -8,7 +8,7 @@ def create_users_table():
     # Create the 'users' table if it doesn't exist
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
-            userId TEXT PRIMARY KEY,
+            id TEXT PRIMARY KEY,
             firstName TEXT,
             middleName TEXT,
             lastName TEXT,
@@ -16,11 +16,72 @@ def create_users_table():
             age INTEGER,
             gender TEXT,
             location TEXT,
-            interests TEXT
+            interests TEXT,
+            password TEXT
         )
     ''')
 
     # Commit and close the connection
     conn.commit()
     conn.close()
+    return
+def create_userLikes_table():
+    conn = sqlite3.connect('tinder.db')
+    cursor = conn.cursor()
+
+    # Create the userLikes table
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS userLikes (
+        id TEXT PRIMARY KEY NOT NULL,
+        userId TEXT NOT NULL,
+        userLikes TEXT NOT NULL,
+        dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
+    )
+''')
+    conn.commit()
+    conn.close()
+    return
+def create_userDislikes_table():
+    conn = sqlite3.connect('tinder.db')
+    cursor = conn.cursor()
+
+    # Create the userDislikes table
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS userDislikes (
+        id TEXT PRIMARY KEY NOT NULL,
+        userId TEXT NOT NULL,
+        userDislikes TEXT NOT NULL,
+        dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
+    )
+''')
+
+    # Commit and close the connection
+    conn.commit()
+    conn.close()
+    return
+def create_userMatches_table():
+    conn = sqlite3.connect('tinder.db')
+    cursor = conn.cursor()
+
+    # Create the userDislikes table
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS userMatches(
+        id TEXT PRIMARY KEY NOT NULL,
+        user1Id TEXT NOT NULL,
+        user2Id TEXT NOT NULL,
+        isMatch INT NOT NULL,
+        dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user1Id) REFERENCES users(id) ON DELETE CASCADE
+    )
+''')
+
+    # Commit and close the connection
+    conn.commit()
+    conn.close()
+    return
 create_users_table()
+create_userLikes_table()
+create_userDislikes_table()
+create_userMatches_table()
